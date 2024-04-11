f5e0062ae9ae6dac9f9e13040b1119338a6ce6f9
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-04-11 22:12:44 +0200

    feat: article item component and basic UI

--- src/admin/routes/articles/page.tsx
+++ src/admin/routes/articles/page.tsx
@@ -1,10 +1,37 @@
 import { RouteConfig } from "@medusajs/admin"
-import { DocumentSeries } from "@medusajs/icons"
+import { DocumentSeries, ArchiveBox, MagnifyingGlass, Funnel, TrianglesMini } from "@medusajs/icons"
+import ArticleItem from "../../../ui-components/article_item";
 
 const ArticlePage = () => {
   return (
-    <div>
-      This is my custom route
+    <div className="flex flex-col gap-9">
+
+        <div className="flex justify-between items-center">
+            <button className="px-3 py-2 rounded bg-green-500 text-white font-medium flex gap-3 items-center">
+                <ArchiveBox/>
+                New article
+            </button>
+            <div className="flex items-center gap-2">
+                <div className="px-3 py-2 flex gap-2 items-center text-white bg-blue-500 rounded font-medium">
+                    <TrianglesMini />
+                    Sort by
+                </div>
+                <div className="px-3 py-2 flex gap-2 items-center text-white bg-yellow-500 rounded font-medium">
+                    <Funnel />
+                    Filter
+                </div>
+                <div className="relative">
+                    <MagnifyingGlass className="absolute left-3 top-1/2 -translate-y-1/2 text-gray-400"/>
+                    <input type="text" className="focus:outline-none border border-gray-300 rounded py-2 pr-3 pl-10 placeholder-gray-400" placeholder="Search" />
+                </div>
+            </div>
+        </div>
+
+        <div className="grid grid-cols-3 gap-x-3 gap-y-4">
+            <ArticleItem />
+            <ArticleItem />
+        </div>
+
     </div>
   )
 }
