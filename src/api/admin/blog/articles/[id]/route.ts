864179ca85491ed8608c18530d6a09f6c82f2808
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-05-29 14:34:39 +0200

    fix: another req type error

--- src/api/admin/blog/articles/[id]/route.ts
+++ src/api/admin/blog/articles/[id]/route.ts
@@ -38,7 +38,8 @@ export const POST = async (req: MedusaRequest, res: MedusaResponse) => {
 }
 
 export const DELETE = async (req: MedusaRequest, res: MedusaResponse) => {
-    const blog_article_id = req.params.id;
+    let anyreq = req as any; // Needed to not receive type errors
+    const blog_article_id = anyreq.params.id;
     
     return res.json({
         success: true
