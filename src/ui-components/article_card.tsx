b9c15454ac24d5c5f2de707acb887ee7fb1e0f87
MoscatelliMarco <marcomoscatelli.dev@gmail.com>
2024-06-02 13:44:49 +0200

    feat: no thumbnail image on  article_card dynamic

--- src/ui-components/article_card.tsx
+++ src/ui-components/article_card.tsx
@@ -4,8 +4,21 @@ export const ArticleCard = (props) => {
     return (
         <Container className="container p-2 rounded-md flex flex-col h-72">
             <a href={`/a/articles/edit/${props.article.id}`}>
-            <div className="w-full h-40 overflow-hidden rounded-md">
-                <img src={`${props.article.thumbnail_image || ""}`} alt="" className="object-cover"/>
+            <div className="w-full h-40 overflow-hidden rounded-md shadow-sm border border-dashed border-black/10">
+                {
+                    props.article.thumbnail_image ?
+                    (
+                        <img 
+                        src={`${props.article.thumbnail_image}`} 
+                        alt="" 
+                        className="object-cover"/>
+                    ) :
+                    (
+                        <div className="grid place-items-center h-full text-black/20 font-light text-sm">
+                            No thumbnail image
+                        </div>
+                    )
+                }
             </div>
             <h3 className="text-lg font-medium">{props.article.title || ""}</h3>
             <p>{props.article.seo_description?.length > 150 ? 
